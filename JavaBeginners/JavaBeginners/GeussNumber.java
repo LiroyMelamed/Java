a23ed13d0ba83ec3cd938751f642926c2d1b1aff
Liroy <zaqwedsxc2@gmail.com>
2021-05-10 18:16:04 +0300

    Final

---
+++ JavaBeginners/JavaBeginners/GeussNumber.java
@@ -0,0 +1,56 @@
+package JavaBeginners;
+
+import java.util.Scanner;
+
+class GeussNumber {
+ public static void geussNumber(int N) {
+  boolean c = true;
+  int max = 100;
+  int min = 0;
+  int number = 50;
+  int num = 50;
+  int MC = 1;
+  while (c) {
+   System.out.println(number);
+   System.out.println("Is it your number?");
+   System.out.println("1)Yes");
+   System.out.println("2)My number is bigger");
+   System.out.println("3)My number is smaller");
+   Scanner myObj = new Scanner(System.in);
+   int n = myObj.nextInt();
+   if (n == 1) {
+    c = false;
+    System.out.println("Win!");
+    System.out.println(MC);
+   }
+   if (n == 2) {
+    min=number;
+    num=number;
+    number = (max + min)/2;
+    MC++;
+   }
+   if (n == 3) {
+    max = number;
+    num = min;
+    number = (max + min) / 2;
+    MC++;
+   }
+  }
+
+ }
+
+ public static void main(String[] args) {
+  System.out.println("Do you want us to guess a number between 1-100?");
+  System.out.println("1)Yes");
+  System.out.println("2)No");
+  Scanner myObj = new Scanner(System.in);
+  int n = myObj.nextInt();
+  if (n == 1) {
+   geussNumber(1000);
+  }
+  if (n == 2) {
+   System.out.println("Your lost");
+  }
+  }
+
+ }
---
+++ JavaMath/TwinPrime.java
@@ -0,0 +1,64 @@
+package JavaMath;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class TwinPrimes {
+
+    public static boolean IsPrime(int num) {
+        boolean Prime = true;
+        if (num <= 1)
+            return false;
+        if (num == 2 || num == 3)
+            return true;
+        if (num % 2 == 0 || num % 3 == 0)
+            return false;
+        for (int i = 5; i <= Math.sqrt(num); i = i + 6) {
+            if (num % i == 0 || num % (i + 2) == 0)
+                return false;
+        }
+        return Prime;
+    }
+
+    public static void DualPrime(int num) {
+        for (int i = 3; i < num; i++) {
+            if (IsPrime(i) && IsPrime(i + 2)) {
+                int[] Dual = { i, i + 2 };
+                System.out.println(Arrays.toString(Dual));
+
+            }
+        }
+    }
+
+    public static void DualPrimeGape(int n, int m) {
+        for (int i = 3; i < n; i++) {
+            if (IsPrime(i) && IsPrime(i + m)) {
+                int[] Dual = { i, i + m };
+                System.out.println(Arrays.toString(Dual));
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.print("Enter a number: ");
+        Scanner myObj = new Scanner(System.in);
+        int num = myObj.nextInt();
+        System.out.println("My number is Prime?");
+        if (IsPrime(num))
+            System.out.println("True");
+        else
+            System.out.println("False");
+
+        System.out.println("All the DualPrime numbers are:");
+        DualPrime(num);
+
+        System.out.print("Enter a number: ");
+        Scanner myObj1 = new Scanner(System.in);
+        int n = myObj1.nextInt();
+        System.out.print("Enter a gape: ");
+        Scanner myObj2 = new Scanner(System.in);
+        int m = myObj2.nextInt();
+        System.out.print("Your numbers are: ");
+
+    }
+}
