21462fc34e5c718d44f88172d0fda3c42847235c
Liroy <zaqwedsxc2@gmail.com>
2021-05-12 21:44:14 +0300

    Final

--- JavaBeginners/IsFormula.java
+++ JavaBeginners/IsFormula.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 class IsFormula {
 
@@ -21,9 +22,56 @@ class IsFormula {
    return true;
  }
 
+ public static double assign(String s, int x) {
+  if (!isFormula(s))
+   throw new Error("Not a Formula");
+  char a = (char) x;
+  for (int i = 0; i < (s.length()); i++) {
+   if (s.charAt(i) == 'x')
+    s.replace('x', a);
+  }
+  System.out.println(s);
+  int len = s.length();
+  ArrayList list1 = new ArrayList();
+  ArrayList list2 = new ArrayList();
+  for (int i = 0; i < len; i++) {
+   if ((s.charAt(i) != '+') && (s.charAt(i) != '-')) {
+    // check if the number is double-digit
+    if ((i + 1 <= len - 1)) {
+     if ((s.charAt(i + 1) != '+') && (s.charAt(i + 1) != '-')) {
+      String temp = "";
+      temp = temp + s.charAt(i) + s.charAt(i + 1);
+      int tempToInt = Integer.parseInt(temp);
+      // adding the double digit number
+      list1.add(tempToInt);
+     }
+     // add single digit number
+     list1.add(s.charAt(i) - '0');
+    }
+   } else {
+    // adding the symbols
+    list2.add(s.charAt(i));
+   }
+  }
+  int result = 0;
+  result = result + (int) list1.get(0);
+  for (int t = 0; t < list2.size(); t++) {
+   char oper = (char) list2.get(t);
+   if (oper == '+') {
+    result = result + (int) list1.get(t + 1);
+
+   } else if (oper == '-') {
+    result = result - (int) list1.get(t + 1);
+   }
+  }
+  return result;
+
+ }
+
  public static void main(String[] args) {
-  String s = "((x*+5)*(x-5))";
-  System.out.println(isFormula(s));
+  String s = "((x+5)*(x-5))";
+  // System.out.println(isFormula(s));
+  System.out.println(assign(s, 2));
  }
 
 }
